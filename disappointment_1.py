a0eb6a978d69f9e5e0bbcf3b83552ed5018f600c
Troy205 <wigleytr@gmail.com>
2025-09-26 12:45:40 +1200

    Disappointment_V1

---
+++ disappointment_1.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# This code achieves nothing but producing an image. There is no real physics here.
+
+size = 800
+x = np.linspace(-1, 1, size)
+y = np.linspace(-1, 1, size)
+X, Y = np.meshgrid(x, y)
+R = np.sqrt(X**2 + Y**2)
+theta = np.arctan2(Y, X)
+
+wavelengths = {
+    "R": 650,
+    "G": 530,
+    "B": 450
+}
+
+thickness_factor = 2000  # changes fringe density
+phase = 2 * np.pi * R * thickness_factor
+
+angular_modulation = np.sin(theta) ** 2
+
+image = np.zeros((size, size, 3))
+
+for i, (color, wl) in enumerate(wavelengths.items()):
+    # Phase shift dependent on wavelength
+    intensity = 0.5 * (1 + np.cos(phase / wl))
+    intensity *= angular_modulation 
+    image[..., i] = intensity
+
+image = image / image.max()
+
+plt.imshow(image, origin="lower", extent=[-1, 1, -1, 1])
+plt.axis("off")
+plt.show()
